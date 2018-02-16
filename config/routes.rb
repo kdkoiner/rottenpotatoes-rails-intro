a0f37276bf0e01529a2a494727c84de00dca3e5e
EC2 Default User <ec2-user@ip-172-31-42-167.us-east-2.compute.internal>
2018-02-16 05:59:23 +0000

    My changes

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   resources :movies
   
-  get 'movies/' => 'movies#sort_test', as: :sort_test_movie
+  get 'movies/1' => 'movies#sort_test', as: :sort_test_movie
   
   # Example resource route with options:
   #   resources :products do
