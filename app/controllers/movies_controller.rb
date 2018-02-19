8835ab538b4e4d5e05a66cbf637483aa0e539c7d
EC2 Default User <ec2-user@ip-172-31-42-167.us-east-2.compute.internal>
2018-02-19 04:13:13 +0000

    part 2 working on

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,6 +14,7 @@ class MoviesController < ApplicationController
    
     @all_ratings = ['G', 'PG', 'PG-13', 'R']
     @selected_ratings = params[:ratings]
+    @ratings_array = @selected_ratings.keys
     @sort = params[:sort]
     @movies = Movie.all
     
