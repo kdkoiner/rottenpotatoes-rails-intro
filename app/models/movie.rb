e65ff4d21277e0f9af122f268c3d4085c7d28e87
kdkoiner <32648384+kdkoiner@users.noreply.github.com>
2018-03-08 15:51:56 -0600

    Updated movie.rb to match heroku
    updated to match heroku

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+    def self.current_ratings
+      	result = {}
+      	self.select(:rating).uniq.each do |movie|
+      		result[movie.rating] = 1
+      	end
+      	return result
+    end
 end
